24548b5a63a9013a4de9883bab1befdbf763387e
shadow3 <1394025007@qq.com>
2023-01-26 19:46:48 +0800

    modified:   Module/Feashelper 	modified:   Module/module.prop 	modified:   src/build/build.sh 	new file:   update.json

--- src/build/build.sh
+++ src/build/build.sh
@@ -2,3 +2,4 @@ rm -rf ./Feashelper
 clang++ ../feashelper/feashelper.cpp -O3 --static -s -o Feashelper --target=arm64 > /dev/null 2>&1
 #fallback
 [ ! -f ./Feashelper ] && arm-linux-gnueabihf-c++ ../feashelper/feashelper.cpp -O3 --static -s -o Feashelper
+[ -f ./Feashelper ] && cp -r ./Feashelper ../../Module/Feashelper
