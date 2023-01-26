8a4c6eb0957d28100b8a6eaba9083997c7ca94fe
shadow3 <1394025007@qq.com>
2023-01-26 23:56:57 +0800

    final v0.8

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,6 +1,5 @@
-MODDIR=${0%/*}
 rm -rf ./Feashelper
 clang++ ../feashelper/feashelper.cpp -O3 --static -s -o Feashelper
 #fallback
 [ ! -f $MODDIR/Feashelper ] && arm-linux-gnueabihf-c++ ../feashelper/feashelper.cpp -O3 --static -s -o ./Feashelper
-[ -f ./Feashelper ] && cp -r $MODDIR/Feashelper ../../Module/Feashelper
+[ -f ./Feashelper ] && cp -r ./Feashelper ../../Module/Feashelper
