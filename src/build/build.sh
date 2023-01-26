cb2b3150282cd9a8c42d6f8d2d2be4a255a998ff
shadow3 <1394025007@qq.com>
2023-01-26 23:51:08 +0800

    she2walt

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,5 +1,6 @@
+MODDIR=${0%/*}
 rm -rf ./Feashelper
 clang++ ../feashelper/feashelper.cpp -O3 --static -s -o Feashelper
 #fallback
-[ ! -f ./Feashelper ] && arm-linux-gnueabihf-c++ ../feashelper/feashelper.cpp -O3 --static -s -o Feashelper
-[ -f ./Feashelper ] && cp -r ./Feashelper ../../Module/Feashelper
+[ ! -f $MODDIR/Feashelper ] && arm-linux-gnueabihf-c++ ../feashelper/feashelper.cpp -O3 --static -s -o ./Feashelper
+[ -f ./Feashelper ] && cp -r $MODDIR/Feashelper ../../Module/Feashelper
