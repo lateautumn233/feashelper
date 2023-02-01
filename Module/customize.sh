bb0e6b4e318c62861ef337e08c9476cc9c04f0ac
shadow3 <1394025007@qq.com>
2023-02-01 16:51:50 +0800

    modified:   Module/customize.sh 	modified:   Module/feas.conf

--- Module/customize.sh
+++ Module/customize.sh
@@ -25,7 +25,10 @@ if [ -f /product/pangu ]; then
 fi
 
 # keep config
-[ ! -f /data/feas.conf ] && cp $MODPATH/feas.conf /data/feas.conf
+# [ ! -f /data/feas.conf ] && cp $MODPATH/feas.conf /data/feas.conf
+mv /data/feas.conf /data/feas.conf.bak
+cp -f $MODPATH/feas.conf /data/feas.conf
+echo "Please note that this update does not preserve the configuration."
 
 # remove model config
 rm $MODPATH/feas.conf
