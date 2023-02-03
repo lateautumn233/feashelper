db654264106f36cb12a87815b5af297ffadd2abd
1394025007@qq.com <1394025007@qq.com>
2023-02-03 23:44:11 +0800

    modified:   Module/Feashelper 	modified:   Module/Feashelper_v1.2-beta.zip 	modified:   build/Feashelper 	modified:   src/Addutils.cpp

--- src/Addutils.cpp
+++ src/Addutils.cpp
@@ -58,34 +58,34 @@ static void qcom(std::string &Frontpkgname)
 static void mtk(std::string &Frontpkgname)
 {
     /* Genshin */
-    if (std::any_of(OP.cbegin(), OP.cend(), [&](const auto &it) { return Frontpkgname == it; }))
+    /*if (std::any_of(OP.cbegin(), OP.cend(), [&](const auto &it) { return Frontpkgname == it; }))
     {
         Lockvalue("/sys/module/mtk_fpsgo/parameters/predict_freq_level", 0);
         Lockvalue("/sys/module/mtk_fpsgo/parameters/normal_frame_keep_count", 5);
         Lockvalue("/sys/module/mtk_fpsgo/parameters/scaling_a", 280);
         Lockvalue("/sys/module/mtk_fpsgo/parameters/scaling_b", -40);
         Lockvalue("/sys/kernel/fpsgo/fbt/switch_idleprefer", 0);
-    }
+    }*/
     /* Glory of Kings */
-    else if (std::any_of(NP.cbegin(), NP.cend(), [&](const auto &it) { return Frontpkgname == it; }))
+    /*else if (std::any_of(NP.cbegin(), NP.cend(), [&](const auto &it) { return Frontpkgname == it; }))
     {
         Lockvalue("/sys/module/mtk_fpsgo/parameters/predict_freq_level", 1);
         Lockvalue("/sys/module/mtk_fpsgo/parameters/normal_frame_keep_count", 12);
         Lockvalue("/sys/module/mtk_fpsgo/parameters/scaling_a", 400);
         Lockvalue("/sys/module/mtk_fpsgo/parameters/scaling_a_thres", 580);
         Lockvalue("/sys/module/mtk_fpsgo/parameters/scaling_b", -75);
-    }
+    }*/
     /* Lolm */
-    else if (Frontpkgname == "com.tencent.lolm")
+    /*else if (Frontpkgname == "com.tencent.lolm")
     {
         Lockvalue("/sys/module/mtk_fpsgo/parameters/predict_freq_level", 1);
         Lockvalue("/sys/module/mtk_fpsgo/parameters/normal_frame_keep_count", 6);
         Lockvalue("/sys/module/mtk_fpsgo/parameters/scaling_a", 365);
         Lockvalue("/sys/module/mtk_fpsgo/parameters/scaling_a_thres", 500);
         Lockvalue("/sys/module/mtk_fpsgo/parameters/scaling_b", -50);
-    }
+    }*/
     /* Bh3 */
-    else if (std::any_of(BP.cbegin(), BP.cend(), [&](const auto &it) { return Frontpkgname == it; }))
+    /*else if (std::any_of(BP.cbegin(), BP.cend(), [&](const auto &it) { return Frontpkgname == it; }))
     {
         Lockvalue("/sys/module/mtk_fpsgo/parameters/predict_freq_level", 1);
         Lockvalue("/sys/module/mtk_fpsgo/parameters/normal_frame_keep_count", 8);
@@ -93,7 +93,7 @@ static void mtk(std::string &Frontpkgname)
         Lockvalue("/sys/module/mtk_fpsgo/parameters/scaling_b", -60);
         Lockvalue("/sys/module/mtk_fpsgo/parameters/load_scaling_x", 5);
         Lockvalue("/sys/module/mtk_fpsgo/parameters/load_scaling_x", 1);
-    }
+    }*/
 }
 
 void addutils(std::string Frontpkgname)
