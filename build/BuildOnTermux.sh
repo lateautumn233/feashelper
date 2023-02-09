e9f08ffef802cd83e994b5642b749aeb885db07d
asto18089 <asto18089@126.com>
2023-02-09 18:34:35 +0800

    Remake all flags Most of the flags added are wrong or not applicable.

--- build/BuildOnTermux.sh
+++ build/BuildOnTermux.sh
@@ -1,3 +1,3 @@
-aarch64-linux-android-clang++ --std=c++20 -Os -fdata-sections -ffunction-sections -fno-threadsafe-statics -fno-omit-frame-pointer -fno-rtti -mcpu=cortex-a78 -D_GNU_SOURCE -flto -fuse-ld=lld -static-libstdc++ -fvisibility=hidden -fvisibility-inlines-hidden -Wl,--icf=all,-O3,--lto-O3,--strip-all -ffixed-x18 -Wl,--hash-style=both -fPIE -Wl,-exclude-libs,ALL -Wl,--gc-sections ../src/*.cpp -o FEAShelper
+aarch64-linux-android-clang++ -Wall -std=c++20 -Os -flto -static -stdlib=libc++ -march=sapphirerapids -fno-rtti -fvisibility=hidden -fvisibility-inlines-hidden -fuse-ld=lld -Wl,-O3,--lto-O3,--gc-sections,--icf=all,-s,-x ../src/*.cpp -o FEAShelper
 sstrip ./FEAShelper; true
 cp -f FEAShelper ../Module/FEAShelper
