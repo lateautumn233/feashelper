6421fbc41f9348f3df502bc5454cd9248de2e93f
asto18089 <asto18089@126.com>
2023-02-09 18:38:04 +0800

    Fix missing copy and paste

--- build/BuildOnTermux.sh
+++ build/BuildOnTermux.sh
@@ -1,3 +1,3 @@
-aarch64-linux-android-clang++ -Wall -std=c++20 -Os -flto -static -stdlib=libc++ -march=sapphirerapids -fno-rtti -fvisibility=hidden -fvisibility-inlines-hidden -fuse-ld=lld -Wl,-O3,--lto-O3,--gc-sections,--icf=all,-s,-x ../src/*.cpp -o FEAShelper
+aarch64-linux-android-clang++ -Wall -std=c++20 -Os -flto -stdlib=libc++ -mcpu=cortex-a78 -fno-rtti -fvisibility=hidden -fvisibility-inlines-hidden -static -fuse-ld=lld -Wl,-O3,--lto-O3,--gc-sections,--icf=all,-s,-x ../src/*.cpp -o FEAShelper
 sstrip ./FEAShelper; true
 cp -f FEAShelper ../Module/FEAShelper
